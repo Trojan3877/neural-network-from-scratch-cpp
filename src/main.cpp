7a9927bfa40dbc00f51f4dfcbc73cb621259ebe6
Corey Leath <corey22blue@hotmail.com>
2025-12-23 18:21:53 -0500

    Create src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "../core/NeuralNet.hpp"
+
+int main() {
+    std::cout << "Neural Network from Scratch (C++) Demo\n";
+    std::cout << "Build successful. Extend with datasets and training loop.\n";
+    return 0;
+}
